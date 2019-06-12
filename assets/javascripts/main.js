db5d6a31ecd183e9bb670797ff23fb1198cdfe18
Barfuss <chris.barfus@positiveaction.net>
2019-06-12 13:13:34 -0600

    New Content Add
    - /giggles added
    - Reviews added to homepage
    - Services added to homepage (WIP, disabled)
    - Some CSS adjustments

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -212,3 +212,32 @@ $('.filterButton').click(function(event) {
 $('.tagsColumn').click(function(event) {
   event.stopPropagation();
 });
+
+
+// Quote Slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("testimony");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
---
+++ giggle.md
@@ -0,0 +1,5 @@
+---
+title: Giggle
+layout: giggle
+permalink: /giggle/
+---
